2c1d5e04040c1309ae69c51b7a16c47d3898b9b2
Gigi <dergigi@pm.me>
2023-01-18 20:58:45 +0100

    Fix quotes.json path

--- fetch-quotes.sh
+++ fetch-quotes.sh
@@ -3,5 +3,5 @@
 # Local: ./bin/fetchquotes.sh from application root directory.
 # Heroku: heroku run ./bin/fetchquotes.sh
 # | python -m json.tool
-curl -u $HTTP_AUTH_USERNAME:$HTTP_AUTH_PASSWORD https://www.bitcoin-quotes.com/quotes.json  > ./src/quotes.json
+curl -u $HTTP_AUTH_USERNAME:$HTTP_AUTH_PASSWORD https://www.bitcoin-quotes.com/quotes.json  > quotes.json
 
