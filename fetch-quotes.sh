8873dbd1e8f1fe1103b30bcab01b57d4c72236d9
Gigi <dergigi@pm.me>
2023-01-18 20:58:00 +0100

    Create fetch-quotes.sh

---
+++ fetch-quotes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Local: ./bin/fetchquotes.sh from application root directory.
+# Heroku: heroku run ./bin/fetchquotes.sh
+# | python -m json.tool
+curl -u $HTTP_AUTH_USERNAME:$HTTP_AUTH_PASSWORD https://www.bitcoin-quotes.com/quotes.json  > ./src/quotes.json
+
